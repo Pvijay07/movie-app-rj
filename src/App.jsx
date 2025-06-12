70f954aceacaa265611cbf4652cfdf66f72fac16
vijay <vijayrajpallapu3210@gmail.com>
2025-06-12 18:57:10 +0530

    jsd

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 
+import { useState } from "react";
 import "./App.css";
-
+const hasLiked=true;
 const Card = ({ title }) => {
+  const [hasLiked,setHasLiked]=useState(false)
+
   return (
-    <div>
+    <div className="card">
       <h2>{title}</h2>
+      <button onClick={()=>{setHasLiked(!hasLiked)}}>
+        {hasLiked ? "❤️" : "🤍"}
+
+      </button>
     </div>
   );
 };
 function App() {
   return (
-    <div>
-      <h2></h2>
+    <div className="card-container">
+      
 
-      <Card title="Bahubali" rating="8.5" isCool={true} />
+      <Card title="Bahubali" rating="8.5" isCool={true} actors={[{name:'Actors'}]} hasLiked={hasLiked}/>
       <Card title="Salaar" rating="8" />
       <Card title="Kalki" rating="9" />
     </div>
